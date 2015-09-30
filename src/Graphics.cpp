fdf41b5cae38b30ec411a10f4571ddf08caa3281
Matthew <matthewbiggins@hotmail.co.uk>
2015-09-30 18:29:38 +0100

    Changes #4 Complete

---
+++ src/Graphics.cpp
@@ -0,0 +1,25 @@
+#include "Graphics.h"
+#include "Common.h"
+
+//Function to initialise OpenGL
+void initOpenGL()
+{
+	//Smooth shading
+	glShadeModel(GL_SMOOTH);
+
+	//clear the background to black
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	//Clear the depth buffer to1.0
+	glClearDepth(1.0f);
+
+	//Enable depth testing
+	glEnable(GL_DEPTH_TEST);
+
+	//The depth test to use
+	glDepthFunc(GL_LEQUAL);
+
+	//Turn on best perspective correction
+
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+}
