0549cf0c6d2588eb59ee142c10d2d40e76a5a1ce
Matthew <matthewbiggins@hotmail.co.uk>
2015-10-15 18:35:32 +0100

    Changes 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Common.h"
 #include "Graphics.h"
 #include "Vertices.h"
+#include "Shader.h"
 
 Vertex verts[]={
 //Front
@@ -77,6 +78,16 @@ void initScene()
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   //Copy Index data to the EBO
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  GLuint vertexShaderProgram = 0;
+  string vsPath = ASSET_PATH + SHADER_PATH + "/simpleVS.glsl";
+  vertexShaderProgram = loadShaderFromFile(vsPath, VERTEX_SHADER);
+  checkForCompileErrors(vertexShaderProgram);
+
+  GLuint fragmentShaderProgram = 0;
+  string fsPath = ASSET_PATH + SHADER_PATH + "/simpleFS.glsl";
+  fragmentShaderProgram = loadShaderFromFile(fsPath, FRAGMENT_SHADER);
+  checkForCompileErrors(fragmentShaderProgram);
 }
 
 void cleanUp()
