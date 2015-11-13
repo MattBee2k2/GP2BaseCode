7c265adde942d2c6f238ba444bfec8bd97c89d0d
MattBee2k2 <matthewbiggins@hotmail.co.uk>
2015-11-13 12:35:37 +0000

    GameObject Compenent added

--- include/GameObject.h
+++ include/GameObject.h
@@ -2,16 +2,41 @@
 #define _GAMEOBJECT_H
 
 #include "Common.h"
+#include "Vertices.h"
+
 
 class GameObject
 {
 public:
+
 	GameObject();
 	~GameObject();
 
-	void addChild(shared_ptr<GameObject> child);
+	void update();
+	void createBuffer(Vertex *pVerts, int numVerts, int *pindices, int numIndices);
+	void loadShader(const string& vsFilename, const string& fsFilename);
+
 private:
-	vector<shared_ptr<GameObject> > m_ChildObjects;
+	GLuint m_VBO;
+	GLuint m_EBO;
+	GLuint m_VAO;
+	GLuint m_ShaderProgram;
+	int m_NoOfVertices;
+	int m_NoOfIndices;
+
+	mat4 m_ModelMatrix;
+	vec3 m_Position;
+	vec3 m_Rotation;
+	vec3 m_Scale;
+
+	vec4 m_AmbientMaterial;
+	vec4 m_DiffuseMaterial;
+	vec4 m_SpecularMaterial;
+	float m_SpecularPower;
+
+protected:
+
+
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,8 @@ GLuint shaderProgram;
 
 MeshData currentMesh;
 
-vec4 ambientMaterialColour=vec4(0.2f,0.2f,0.2f,1.0f);
 vec4 ambientLightColour=vec4(1.0f,1.0f,1.0f,1.0f);
+vec4 ambientMaterialColour = vec4(0.2f, 0.2f, 0.2f, 1.0f);
 
 vec4 diffuseLightColour=vec4(1.0f,1.0f,1.0f,1.0f);
 vec4 diffuseMaterialColour=vec4(1.0f,0.0f,0.0f,1.0f);
