1de8b04d2263eb15620df75b5e71f70f6fc96d01
Matthew <matthewbiggins@hotmail.co.uk>
2015-11-16 16:47:49 +0000

    Changes #1

--- include/Common.h
+++ include/Common.h
@@ -2,5 +2,13 @@
 #define _COMMON_H
 
 #include <iostream>
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+#ifndef _APPLE_
+#include <OpenGL/glu.h>
+#elif (_WIN64 || _WIN32)
+#include <gl\GLU.h>
+#endif
 
 #endif
